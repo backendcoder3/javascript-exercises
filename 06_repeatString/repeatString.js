9121d1ddb1fe43500517bfcc99d0f3ab33c8d5a0
64M4110 <170906715+backendcoder3@users.noreply.github.com>
2025-10-28 16:32:58 -0600

    Se pasaron los test

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,21 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
 
+    if (num < 0) {
+        return 'ERROR';
+    }
+
+    let resultado = "";
+
+    let contador = 1;
+
+    while (contador <= num) {
+        resultado += string;
+        contador++;
+    }
+    
+    return resultado;
 };
 
 // Do not edit below this line
 module.exports = repeatString;
+
