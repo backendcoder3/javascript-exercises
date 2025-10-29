80655d003b7c887aac3e062f4e18ef65f085e3e4
64M4110 <170906715+backendcoder3@users.noreply.github.com>
2025-10-28 22:36:10 -0600

    Se pasaron los test

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(text) {
+  const arrayDeCaracteres = text.split(''); 
 
+  const arrayInvertido = arrayDeCaracteres.reverse(); 
+
+  const cadenaInvertida = arrayInvertido.join(''); 
+
+  return cadenaInvertida;
 };
 
 // Do not edit below this line
